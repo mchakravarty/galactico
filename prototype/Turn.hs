eec7e5a7f6e0a5222031cad28f7aea93016541b3
Gabriele Keller <keller@cse.unsw.edu.au>
2013-08-26 21:20:51 +1000

    Implemented plot bidding

--- prototype/Turn.hs
+++ prototype/Turn.hs
@@ -4,10 +4,13 @@ module Turn (
   endOfTurn
 ) where
 
+
+import Data.List (partition, groupBy)
+import Control.Monad (liftM)
+
   -- friends
 import World
 import Prelude hiding (round)
-
 -- Turn progression
 -- ----------------
 
@@ -31,18 +34,69 @@ round :: World -> IO World
 round world 
   = do
     { putStrLn "next round"
-    ; world' <- foldM world turn [PlayerA .. PlayerD]
+    ; securedPlots <- resolvePlotBidding world
+    ; world'  <- foldM world executeTurn securedPlots
+    ; world'' <- worldStep world'
     ; return world
     }
   
+-- Assign a plot to each player according to wish list, resolve conflicts
+-- randomly
+resolvePlotBidding :: World -> IO [(PlayerId, TileIndex)]  
+resolvePlotBidding world 
+  = error "resolvePlotBidding not implemented yet"
+  -- apply assignPlots to world, update world accordingly
+  where
+  	-- return a list of players with their assigned plots, and a list of players who 
+    -- lost out
+    assignPlot :: [(PlayerId, TileIndex)] -> IO ([(PlayerId, TileIndex)], [PlayerId])
+    assignPlot choices = do 
+      { let groupChoices = groupBy (\a -> \b -> snd a == snd b) choices
+      ; let (noConflicts, conflicts)  = partition ((==1) . length) groupChoices
+      ; (winners, losers) <- unzipM $ mapM chooseRandom $ map (map fst) conflicts
+      ; let winnerPlots = zipWith (\w -> \g -> (w, snd $ head' g)) winners conflicts
+      ; return (winnerPlots, concat losers)
+      }
 
-turn :: PlayerId -> World -> IO World
-turn playerId world 
+    head' :: [(PlayerId, TileIndex)] -> (PlayerId, TileIndex)
+    head' []    = error $ "Turn.resolvePlotBidding applying head to empty list, impossible"
+    head' (x:_) = x
+
+    -- choose a random player from a list of ids, return succesfull and failed players
+    chooseRandom :: [PlayerId] -> IO (PlayerId, [PlayerId])
+    chooseRandom (p:ps) = return (p, ps) -- todo: proper random
+
+    assignPlots :: [(PlayerId, [TileIndex])] -> IO [(PlayerId, TileIndex)] 
+    assignPlots [] = return []
+    assignPlots playerPlots = do 
+      { let firstChoices = map getFirstChoice playerPlots
+      ; (assigns, unsuccessfullPlayers) <- assignPlot firstChoices
+      ; otherAssigns <- assignPlots $ zipPlots unsuccessfullPlayers playerPlots
+      ; return (assigns ++ otherAssigns)
+      }
+
+    getFirstChoice :: (PlayerId, [TileIndex]) -> (PlayerId, TileIndex)
+    getFirstChoice (playerId, (p:ps)) = (playerId, p)
+    getFirstChoice (playerId, []) 
+      = error $  "Turn.resolvePlotBidding: player " ++ (show playerId) ++
+        " has insufficient number of plot choices in plan"
+
+    zipPlots [] _ = []
+    zipPlots p@(p1:prs) ((p1',pl1'): pls) 
+      = if (p1 == p1')
+          then (p1, tail pl1') : (zipPlots prs pls)
+          else zipPlots p pls      
+
+executeTurn :: (PlayerId, TileIndex) -> World -> IO World
+executeTurn (playerId, tileId) world 
   = do
-    { putStrLn $ (show playerId) ++"'s turn"
+    { putStrLn $ (show playerId) ++" buys plot " ++ (show tileId)
     ; return world
     }  
 
+-- Apply random world events, calculate field output
+worldStep :: World -> IO World
+worldStep world = return world
 
 
 foldM :: Monad m => b -> (a -> b -> m b) -> [a] -> m b
@@ -52,3 +106,6 @@ foldM b f (x:xs)
   { b' <- f x b 
   ; foldM b' f xs
   }
+
+unzipM :: Monad m => m [(a,b)] -> m ([a], [b])
+unzipM = liftM unzip 
